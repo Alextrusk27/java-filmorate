daf5265dfa321d8de0f155f3f4e059bc23f7c265
Nikolay Aleksandrov <nkl.aleks@yandex.ru>
2025-08-26 12:49:53 +0300

    ФЗ 10

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Builder
+@Data
 public class Film {
+    Integer id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Long duration;
 }
